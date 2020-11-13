a25096a3fed14cc73903012150c9507d2706cfb9
[luca cave s3787946] <[s3787946@student.rmit.edu.au]>
2020-11-14 03:02:13 +1100

    Added JWT user auth and adde vim to docker image

--- buildlog_server/config/routes.rb
+++ buildlog_server/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  post "/login", to: "users#login"
+  get "/login", to: "users#auto_login"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

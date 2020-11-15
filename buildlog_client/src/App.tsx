08b0ae87eeb7a4aa49cf4c20e19d6442998ee159
[luca cave s3787946] <[s3787946@student.rmit.edu.au]>
2020-11-15 14:15:20 +1100

    Added animations and remember to login page

--- buildlog_client/src/App.tsx
+++ buildlog_client/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './app/Header';
 import Login from './users/Login';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Login />
     </div>
   );
--- buildlog_client/src/users/Login.tsx
+++ buildlog_client/src/users/Login.tsx
@@ -1,51 +1,72 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import { animated, useTransition } from "react-spring";
 import axios from 'axios';
 import { login } from '../utils/auth/login';
 import { API_URL } from '../utils/env';
-import loading from '../assets/images/loading.gif';
+import LoadingButton from '../app/LoadingButton';
+import useComponentSize from '@rehooks/component-size';
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [remember, setRemember] = useState(false);
+
+    const [submit, toggleSubmit] = useState(false);
+    const [error, toggleError] = useState(false);
+    const ref = useRef(null);
+    const { height } = useComponentSize(ref);
+
+    const expand = useTransition(error, null, {
+        from: { height: 0 },
+        enter: { height },
+        leave: { height: 0 },
+        update: { height }
+    });
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        var btn = document.getElementById("submit-btn")
-        btn!.style.opacity = "0";
-        btn!.style.display = "none";
-        var wheel = document.getElementById("loading-wheel");
-        wheel!.style.display = "inherit";
-        wheel!.style.opacity = "1";
+        if (error) { toggleError(false) };
+        toggleSubmit(true);
         axios.post(API_URL + "/login", {
             "email": email,
             "password": password
         }).then(function (response) {
             login(response.data.token)
         }).catch(function () {
-            wheel!.style.opacity = "0";
-            wheel!.style.display = "none";
-            document.getElementById('error-wrapper')!.style.opacity = "1";
-            btn!.style.display = "inherit";
-            btn!.style.opacity = "1";
+            toggleError(true);
+            toggleSubmit(false);
         })
     }
 
     return (
         <div className="absolute w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
             <div className="p-10 bg-white rounded-lg text-center shadow-lg">
-                <h1 className="text-3xl mb-4">Login</h1>
-                <div className="opacity-0 transition-all duration-300" id="error-wrapper">
-                    <span className="bg-red-300 p-2 rounded-lg">Invalid email or password</span>
-                </div>
-                <form onSubmit={handleSubmit} className="flex flex-col text-left my-4">
+                <h1 className="text-3xl">Login</h1>
+                {expand.map(
+                    ({ item, props, key }) =>
+                        item && (
+                            <animated.div className="bg-red-300 rounded-lg flex justify-center items-center" key={key} id="error-wrapper" style={{
+                                ...props,
+                                overflow: "hidden",
+                                position: "relative"
+                            }}>
+                                <span ref={ref} className="p-2" >Invalid email or password</span>
+                            </animated.div>
+                        ))}
+                <form onSubmit={handleSubmit} id="login-form" className="flex flex-col text-left my-4">
                     <label className="text-blue-700 text-lg ml-2" >Email</label>
                     <input className="mb-4 bg-gray-200 p-2 rounded-lg outline-none border-transparent border-2 focus:border-blue-700" autoFocus type="text" name="email" onChange={e => setEmail(e.target.value)} />
                     <label className="text-blue-700 text-lg ml-2">Password</label>
                     <input className="mb-4 bg-gray-200 p-2 rounded-lg outline-none border-transparent border-2 focus:border-blue-700" type="text" name="password" onChange={e => setPassword(e.target.value)} />
-                    <div className="full-width mt-4 flex justify-center" style={{ height: "45px" }}>
-                        <input type="submit" id="submit-btn" className="p-2 rounded-lg text-lg hover:bg-green-400 bg-green-200 transition-all duration-300 outline-none border-transparent border focus:border-blue-700" />
-                        <img src={loading} id="loading-wheel" className="hidden opacity-0 transition-all duration-200" style={{ height: "45px"}} alt="loading wheel" />
+                    <div className="flex mb-6 ml-2">
+                        <label className="block">
+                            <input className="mr-2 leading-tight" type="checkbox" onChange={e => setRemember(!remember)}/>
+                            <span className="text-sm">
+                                Stay signed in
+                                </span>
+                        </label>
                     </div>
+                    <LoadingButton className="button p-2 rounded-lg text-lg hover:bg-green-400 bg-green-200 transition-all duration-300 outline-none border-transparent border focus:border-blue-700" type="submit" isLoading={submit}> Submit </LoadingButton>
                 </form>
             </div>
         </div>
